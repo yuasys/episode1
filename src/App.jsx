7cc816571f215815c6a570ae6c4e4f8a03993969
Nob Yuasys <yuasys@gmail.com>
2022-11-02 14:22:42 +0900

    Add Cat-Fact button

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import "./App.css";
-import { useState } from "react";
-import { Text } from "./Text";
+import Axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [catFact, setCatFact] = useState("");
+
+  const fetchCatFact = () => {
+    Axios.get("https://catfact.ninja/fact").then((res) => {
+      setCatFact(res.data.fact);
+    });
+  };
+
+  useEffect(() => {
+    fetchCatFact();
+  }, [])
+
+
   return (
     <div className="App">
-      <button>
-        Generate Cat Fact
-      </button>
-      <p></p>
+      <button onClick={fetchCatFact}>Generate Cat Fact</button>
+      <p>{catFact}</p>
     </div>
   );
 }
