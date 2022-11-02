0c19af184c5c307b027a0560dbe210007c810c87
Nob Yuasys <yuasys@gmail.com>
2022-11-02 17:07:48 +0900

    PredictAge added

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,32 @@ import Axios from "axios";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [catFact, setCatFact] = useState("");
-
-  const fetchCatFact = () => {
-    Axios.get("https://catfact.ninja/fact").then((res) => {
-      setCatFact(res.data.fact);
-    });
+  const [name, setName] = useState("");
+  const [predictedAge, setPredicteAge] = useState(null);
+  const fetchData = () => {
+    Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
+      setPredicteAge(res.data);
+    })
   };
 
   useEffect(() => {
-    fetchCatFact();
+    fetchData();
   }, [])
 
 
   return (
     <div className="App">
-      <button onClick={fetchCatFact}>Generate Cat Fact</button>
-      <p>{catFact}</p>
+      <input
+        placeholder="例. Pedro"
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
+        />
+      <button onClick={fetchData}>Predict Age</button>
+      <h1>名前:{predictedAge?.name}</h1>
+      <h1>占い年齢:{predictedAge?.age}</h1>
+      <h1>カウント:{predictedAge?.count}</h1>
+      
     </div>
   );
 }
