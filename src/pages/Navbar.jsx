a9574c893364c7c1a4f2f18d8f2a9672a64e9c71
Nob Yuasys <yuasys@gmail.com>
2022-11-03 10:55:38 +0900

    Add Navbar

---
+++ src/pages/Navbar.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export const Navbar = () => {
+    return (
+        <div>
+        <Link to="/"> Home </Link>
+        <Link to="/menu"> Menu </Link>
+        <Link to="/contact"> Contact</Link>
+      </div>
+
+    );
+}
