6fa5a7f4d2b875d9e1b1608907f6d3bfc8eb171e
Nob Yuasys <yuasys@gmail.com>
2022-11-03 09:21:55 +0900

    Add HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,16 @@
 import "./App.css";
-import Axios from "axios";
-import { useEffect, useState } from "react";
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from "./pages/Home";
 
 function App() {
-  const [generatedExcuse, setGneratedExcuse] = useState("");
-  
-  const fetchExcuse = (excuse) => {
-    Axios.get(`https://excuser.herokuapp.com/v1/excuse/${excuse}/`).then((res) => {
-      setGneratedExcuse(res.data[0].excuse);
-    })
-  };
-
-  return (
+  return 
     <div className="App">
-      <h1>言い訳の作成</h1>
-      <button onClick={() => fetchExcuse("party")}> パーディ欠席</button>
-      <button onClick={() => fetchExcuse("family")}> 家族に言い訳</button>
-      <button onClick={() => fetchExcuse("office")}> 勤務をサボる</button>
-
-      <p> {generatedExcuse} </p>
-    </div>
-  );
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+        </Routes>
+      </Router>
+    </div>;
 }
 
 export default App;
