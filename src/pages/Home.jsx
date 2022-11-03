8e8c7e9aab83ec14056df066c5d58e7433ae486a
Nob Yuasys <yuasys@gmail.com>
2022-11-03 17:46:30 +0900

    ChangeUserName by useState

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,3 @@
-export const Home = () => {
-    return <h1> THIS IS THE HOME PAGE </h1>
+export const Home = (props) => {
+    return <h1> This is the home pege and user is: {props.username} </h1>
 };
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,3 +1,11 @@
-export const Profile = () => {
-    return <h1> PROFILE </h1>
+import { ChangeProfile } from "../components/ChangeProfile";
+
+export const Profile = (props) => {
+    return (
+        <div>
+            {" "}
+            PROFILE, user is: {props.username}
+            <ChangeProfile setUsername={props.setUsername} />
+        </div>
+    );
 };
